4785caaee85cd2df9a680c9387352c8e0f9cda33
Icaro Félix <ifeme08@gmail.com>
2023-11-30 02:43:23 -0300

    Basket microservices developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public string Username { get; set; } = string.Empty;
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.Username, JsonSerializer.Serialize(cart));
+
+            return await GetBasket(cart.Username);
+        }
+    }
+}
